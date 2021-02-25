a9b1f3f0610796c77a6644dde143b13ba46c5780
mewiemers <m.wiemers01@gmail.com>
2021-02-25 14:41:31 +0100

    add rest api endpoints

---
+++ server/db.ts
@@ -0,0 +1,19 @@
+import fs from "fs/promises";
+import { TrackfromApi } from "../utils/api";
+
+type DB = {
+  tracks: TrackfromApi[];
+};
+
+const DB_PATH = "./server.db.json";
+
+export async function readDB(): Promise<DB> {
+  const dbJSON = await fs.readFile("./server/db.json", "utf-8");
+  const db: DB = JSON.parse(dbJSON);
+  return db;
+}
+
+export async function writeDB(db: DB) {
+  const dbJSON = JSON.stringify(db, null, 2);
+  await fs.writeFile(DB_PATH, dbJSON);
+}
